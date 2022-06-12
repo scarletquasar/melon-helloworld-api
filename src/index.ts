e80cba9e9c4902c2dc032ac53cc73d81ac513949
Victória Rose <70824102+EternalQuasar0206@users.noreply.github.com>
2022-06-12 16:04:53 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 const app = http.app()
 
 function helloWorld() {
-    return http.result(200, a.elements().join(" "))
+    return http.result(200, "Hello World!")
 }
-
-const a = new Enumerable<string>(["Hello", " World!"])
 
 app.get("/", "helloWorld")
 app.run()
+
+
